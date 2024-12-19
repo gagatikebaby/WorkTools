e6a54d35721f226ed0c142198c4c94caf52e9d68
gaolh <407259222@qq.com>
2024-12-20 00:09:30 +0800

    updata

---
+++ Model/WindowCard.cs
@@ -0,0 +1,23 @@
+﻿using Wpf.Ui.Controls;
+
+namespace WorkToolsSln.Model
+{
+    public record WindowCard
+    {
+        public string Name { get; set; }
+
+        public string Description { get; init; }
+
+        public SymbolRegular Icon { get; init; }
+
+        public string Value { get; set; }
+
+        public WindowCard(string name, string description, SymbolRegular icon, string value)
+        {
+            Name = name;
+            Description = description;
+            Icon = icon;
+            Value = value;
+        }
+    }
+}
