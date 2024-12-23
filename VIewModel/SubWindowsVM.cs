8625b1c5d0a334e8c2e75ea116c10f3241eb7c0a
gaolh <407259222@qq.com>
2024-12-23 22:33:58 +0800

    add config path

--- VIewModel/SubWindowsVM.cs
+++ VIewModel/SubWindowsVM.cs
@@ -15,41 +15,67 @@ namespace WorkToolsSln.VIewModel
     public partial class SubWindowsVM : ViewModel
     {
         [ObservableProperty]
-        private string dependencyPath = string.Empty;
+        private string _dependencyPath = string.Empty;
+
         [ObservableProperty]
-        private string _openedFolderPath = string.Empty;
+        private string _unpackagePath = string.Empty;
 
 
         private readonly string ConfigPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configuration", "PathConfig.json");
         private PathConfigInfo _PathList = new PathConfigInfo();
         public SubWindowsVM()
         {
-            _PathList =  FileOperation.ReadConfig<PathConfigInfo>(ConfigPath);
-            OpenedFolderPath = _PathList.DependencyPath;
+            _PathList = FileOperation.ReadConfig<PathConfigInfo>(ConfigPath);
+            DependencyPath = _PathList.DependencyPath;
+            UnpackagePath = _PathList.UnpackagePath;
         }
 
         [RelayCommand]
         public void OnSelectDependcyPath()
         {
+            string ret = SelectFolder();
+            if (string.IsNullOrEmpty(ret))
+            {
+                return;
+            }
+            DependencyPath = ret;
+            _PathList.DependencyPath = DependencyPath;
+            FileOperation.WriteConfig<PathConfigInfo>(ConfigPath, _PathList);
+        }
 
+        public void OnSelectUnpackagePath()
+        {
+            string selsctpath = SelectFolder();
+
+            if (string.IsNullOrEmpty(selsctpath))
+            {
+                return;
+            }
+            UnpackagePath = selsctpath;
+            _PathList.UnpackagePath = UnpackagePath;
+            FileOperation.WriteConfig<PathConfigInfo>(ConfigPath, _PathList);
+        }
+
+        private string SelectFolder()
+        {
             OpenFolderDialog openFolderDialog = new()
             {
-                Multiselect = true,
+                Multiselect = false,
                 InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
             };
 
-                    if (openFolderDialog.ShowDialog() != true)
+            if (openFolderDialog.ShowDialog() != true)
             {
-                return;
+                return null;
             }
 
             if (openFolderDialog.FolderNames.Length == 0)
             {
-                return;
+                return null;
             }
 
-            OpenedFolderPath = string.Join("\n", openFolderDialog.FolderNames);
+            // 返回选中的路径
+            return string.Join("\n", openFolderDialog.FolderNames);
         }
-
     }
 }
