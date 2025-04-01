27ae744410d1878a2a35deb4af88581f2abafd07
Gary <a407259222@163.com>
2025-04-01 14:38:18 +0800

    +snow

--- View/SettingPage.xaml.cs
+++ View/SettingPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using WorkToolsSln.Effects;
 using WorkToolsSln.VIewModel;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
@@ -11,14 +12,26 @@ namespace WorkToolsSln.View
     public partial class SettingPage : INavigableView<SettingVM>
     {
         public SettingVM ViewModel { get; }
-
+        private SnowflakeEffect? _snowflake;
         public SettingPage(SettingVM viewModel)
         {
             ViewModel = viewModel;
             DataContext = this;
-
             InitializeComponent();
+            this.Loaded += HandleLoaded;
+            this.Unloaded += HandleUnloaded;
         }
 
+        private void HandleUnloaded(object sender, RoutedEventArgs e)
+        {
+            _snowflake?.Stop();
+            _snowflake = null;
+        }
+
+        private void HandleLoaded(object sender, RoutedEventArgs e)
+        {
+            _snowflake ??= new(MainCanvas);
+            _snowflake.Start();
+        }
     }
 }
